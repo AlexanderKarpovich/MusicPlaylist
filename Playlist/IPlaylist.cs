eaa1d7bb0a51e28ed35ddd4ab2218cb58730f427
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-02-27 17:29:24 +0300

    Comments added.

--- Playlist/IPlaylist.cs
+++ Playlist/IPlaylist.cs
@@ -1,13 +1,37 @@
 namespace Playlist
 {
+    /// <summary>
+    /// Represents playlist interface.
+    /// </summary>
     public interface IPlaylist
     {
+        /// <summary>
+        /// Starts playback of the current song.
+        /// </summary>
+        /// <returns>The task object representing the asynchronous operation.</returns>
         Task Play();
+        /// <summary>
+        /// Stops playback of the current song.
+        /// </summary>
         void Pause();
 
+        /// <summary>
+        /// Switches the song to the previous one.
+        /// </summary>
+        /// <returns>The task object representing the asynchronous operation.</returns>
         Task Prev();
+
+        /// <summary>
+        /// Switches to the next song.
+        /// </summary>
+        /// <returns>The task object representing the asynchronous operation.</returns>
         Task Next();
 
+        /// <summary>
+        /// Adds the specified <see cref="Song" /> to the end of the playlist.
+        /// </summary>
+        /// <param name="lastSong">Specified value of the <see cref="Song" />.</param>
+        /// <returns>The task object representing the asynchronous operation.</returns>
         Task AddSong(Song lastSong);
     }
 }
