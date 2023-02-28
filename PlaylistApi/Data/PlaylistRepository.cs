e28ec2329accf59bc7caadad9dbe9a0826790534
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-02-28 14:31:47 +0300

    Playlist repository added.

---
+++ PlaylistApi/Data/PlaylistRepository.cs
@@ -0,0 +1,69 @@
+using Playlist;
+
+namespace PlaylistApi.Data
+{
+    public class PlaylistRepository : IPlaylistRepository
+    {
+        private readonly PlaylistDbContext context;
+
+        public PlaylistRepository(PlaylistDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void AddSong(Song song)
+        {
+            if (song is null)
+            {
+                throw new ArgumentNullException("song");
+            }
+
+            context.Add(song);
+        }
+
+        public IEnumerable<Song> GetAllSongs()
+        {
+            return context.Songs;
+        }
+
+        public Song? GetSongById(int id)
+        {
+            return context.Songs.Find(id);
+        }
+
+        public void RemoveSong(int id)
+        {
+            Song? song = context.Songs.Find(id);
+            if (song is not null)
+            {
+                context.Songs.Remove(song);
+            }
+        }
+
+        public bool UpdateSong(Song song)
+        {
+            if (song is null)
+            {
+                throw new ArgumentNullException("song");
+            }
+
+            if (!SongExists(song.Id))
+            {
+                return false;
+            }
+            
+            context.Songs.Update(song);
+            return true;
+        }
+
+        public bool SongExists(int id)
+        {
+            return context.Songs.Find(id) is not null;
+        }
+
+        public bool SaveChanges()
+        {
+            return (context.SaveChanges() > 0);
+        }
+    }
+}
