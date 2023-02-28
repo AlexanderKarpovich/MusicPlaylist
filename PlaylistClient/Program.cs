27b97198f1e947cae24e8d533c43cc295b144529
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-01 00:23:19 +0300

    Automapper and gRPC client added to the container.

--- PlaylistClient/Program.cs
+++ PlaylistClient/Program.cs
@@ -1,15 +1,17 @@
+using PlaylistClient.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IPlaylistDataClient, PlaylistDataClient>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
